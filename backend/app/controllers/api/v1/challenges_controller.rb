6063fe5913044dbea4642a1446efccb9f9d61289
zehan12 <zehan9211@gmail.com>
2024-10-24 17:06:26 +0530

    add controller function definations

---
+++ backend/app/controllers/api/v1/challenges_controller.rb
@@ -0,0 +1,35 @@
+module Api
+  module V1
+    class ChallengesController < ApplicationController
+      # GET /api/v1/challenges
+      def index
+        # show all challenges
+      end
+
+      # POST /api/v1/challenges
+      def create
+        # create a single challenges
+      end
+
+      # GET /api/v1/challenges/:id
+      def show
+        # show single challenges
+      end
+
+      # PUT /api/v1/challenges/:id
+      def update
+        # update single challenges
+      end
+
+      # PATCH /api/v1/challenges/:id
+      def update_title
+        # update single challenge title
+      end
+
+      # DELETE /api/v1/challenges/:id
+      def destroy
+        # delete single challenge
+      end
+    end
+  end
+end
