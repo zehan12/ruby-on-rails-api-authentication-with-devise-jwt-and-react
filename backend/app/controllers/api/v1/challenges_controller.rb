0eb7c7d23664c92e8b38eb2ebcd7781a85fed13a
zehan12 <zehan9211@gmail.com>
2024-10-25 02:41:07 +0530

    add controller logic and move forward to auth part

--- backend/app/controllers/api/v1/challenges_controller.rb
+++ backend/app/controllers/api/v1/challenges_controller.rb
@@ -4,11 +4,27 @@ module Api
       # GET /api/v1/challenges
       def index
         # show all challenges
+        @challenges = Challenge.all
+        render json: @challenges
       end
 
       # POST /api/v1/challenges
       def create
         # create a single challenges
+        puts params
+        # challenge = Challenge.new(
+        #   title: "my rails app " + Date.today.to_s,
+        #   description: "my description",
+        #   start_date: Date.today,
+        #   end_date: Date.tomorrow
+        # )
+
+        # if challenge.save
+        #   render json: { message: "Challenges added successfully!", data: challenge }
+        # else
+        #   render json: { message: "Failed to add challenges", data: challenge.errors }
+        # end
+        render json: { message: "Hello world" }
       end
 
       # GET /api/v1/challenges/:id
