85c0d1b4a789178f6573635449599cc71cd16d44
zehan12 <zehan9211@gmail.com>
2024-10-25 13:41:13 +0530

    add devise generate step action mailer

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # TODO [prod]
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
